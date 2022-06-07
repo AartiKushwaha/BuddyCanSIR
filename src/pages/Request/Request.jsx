603609421605006ab6281aac9d6546b1010dfdec
Aarti Kushwaha <rtkush1211@gmail.com>
2022-06-07 15:48:48 +0530

    request page

--- src/pages/Request/Request.jsx
+++ src/pages/Request/Request.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./request.css";
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
+import Form from "./Form";
 
 export default function Request() {
-    return(
-        <>
+    return (
+      <>
         <Navigation />
-        <h1>Request Page</h1>
+        <Form />
         <Footer />
-        </>
+      </>
     );
 }
