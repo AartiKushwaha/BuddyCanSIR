286a740e3a509b557dee4ee401cb30466a256fdb
Manisha Chaturvedi <68684840+manisha1904@users.noreply.github.com>
2022-06-07 22:00:58 +0530

    Update Form.js

--- src/pages/Request/Form.js
+++ src/pages/Request/Form.js
@@ -29,7 +29,7 @@ function Form() {
   };
 
   return (
-    <div className="form mt-5">
+    <div className="form">
       <div className="progressbar">
         <div
           style={{ width: page === 0 ? "33.3%" : page === 1 ? "66.6%" : "100%" }}
