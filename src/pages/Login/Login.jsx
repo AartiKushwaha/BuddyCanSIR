89451c648be0cfae29876985ca199950d1c7c332
Punerva Singh <punerva@Punervas-MacBook-Pro.local>
2022-06-07 16:46:15 +0530

    Added login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,11 +4,38 @@ import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
 
 export default function Login() {
-    return(
+    return (
         <>
-        <Navigation />
-        <h1>Login Page</h1>
-        <Footer />
+            <Navigation />
+            <div className="login">
+                <div className="login-container">
+                    <p className="login-heading">User Login</p>
+                    <form className="login-form">
+                        <label className="login-label">
+                            Phone No./Email: <br />
+                        </label>
+                        <input type="email" name="email" placeholder="Enter your email/phone no."/>
+                        <br />
+                        <label className="login-label">
+                            Password: <br />
+                        </label>
+                        <input type="password" name="password" placeholder="Enter your password"/>
+                        <br />
+                        <p className="disclaimer">* Password is your uid.</p>
+                        <input type="submit" value="Login" />
+                    </form>
+                    <a href="#">Forgot Password?</a>
+                    <hr style={{margin: "10px 0"}} />
+                    <button className="login-button">Login using Google</button>
+                    <br />
+                    <button className="login-button">Login using OTP</button>
+                </div>
+            <div className="circle1"></div>
+            <div className="circle2"></div>
+            <div className="circle3"></div>
+            <div className="circle4"></div>
+            </div>
+            <Footer />
         </>
     );
 }
