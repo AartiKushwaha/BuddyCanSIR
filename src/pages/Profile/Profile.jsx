13b2f5736d411ec8a0da4762d10ad6438d852627
Prachi Aggarwal <prachi2002aggarwal@gmail.com>
2022-06-08 21:24:14 +0530

    profile-updated

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -16,16 +16,56 @@ export default function Profile() {
                 <div className="card-body text-center">
                   <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" className="rounded-circle img-fluid" style={{width: '150px'}} />
                   <h5 className="my-3">John Smith</h5>
-                  <p className="text-muted mb-1">A1234</p>
-                  <p className="text-muted mb-1">example@example.com</p>
-                  <p className="text-muted mb-1">(097) 234-5678</p>
-                  <p className="text-muted mb-1">July 10, 2001</p>
+                  <br />
+                  <form className="uploadImage">
+                    <h6>Upload Profile Image</h6>
+                    <input type="file" name="myImage" accept="image/x-png,image/gif,image/jpeg" />
+                  </form>
+                  {/*<p className="text-muted mb-1">example@example.com</p>*/}
+                  {/*<p className="text-muted mb-1">(097) 234-5678</p>*/}
+                  {/*<p className="text-muted mb-1">July 10, 2001</p>*/}
                 </div>
               </div>
             </div>
             <div className="col-lg-8">
               <div className="card mb-4">
                 <div className="card-body">
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Name</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">John Smith</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Email ID</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">example@example.com</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Contact No.</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">(097) 234-5678</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Date of Birth</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">July 10, 2001</p>
+                    </div>
+                  </div>
+                  <hr />
                   <div className="row">
                     <div className="col-sm-3">
                       <p className="mb-0">Education Level</p>
@@ -62,18 +102,18 @@ export default function Profile() {
                     </div>
                   </div>
                   <hr />
-                  
                 </div>
-                <a href="#" class="btn btn-info btn-lg">
-          <span class="glyphicon glyphicon-pencil"></span> Edit
-        </a>
+                <div className="editButton">
+                          <a href="#" className="btn btn-info btn-lg">
+                    <span className="glyphicon glyphicon-pencil"></span> Edit
+                            <img className="img-short" src={require("../../assets/img/editIcon.png")} alt="..."/>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-        
-        
         <Footer />
         </>
     );
